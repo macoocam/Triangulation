72ff727f7f4dc2ea821a41ea287110906a7ee2bb
macoocam <homeback19537@gmail.com>
2017-08-02 01:45:34 +0800

    常駐thread和layout

--- sample_app/src/main/java/uk/co/alt236/btlescan/activities/MainActivity.java
+++ sample_app/src/main/java/uk/co/alt236/btlescan/activities/MainActivity.java
@@ -61,8 +61,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected ListView mList;
     @Bind(android.R.id.empty)
     protected View mEmpty;
-
-
     @Bind(R.id.tv_estimates_x)
     protected TextView mestimates_x;
     @Bind(R.id.tv_estimates_y)
@@ -75,8 +73,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected EditText mrealx;
     @Bind(R.id.ed_realy)
     protected EditText mrealy;
-
-
     @Bind(R.id.bt_calculate)
     protected Button mcalculate;
     @Bind(R.id.receive_bt)
@@ -90,9 +86,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //    protected RelativeLayout drawV;
 
     DrawView drawView;
-    //    CountObj c1 = new CountObj(), c2 = new CountObj(), c4 = new CountObj(), c5 = new CountObj(), c6 = new CountObj(), c9 = new CountObj(),
-//            c11 = new CountObj(), c12 = new CountObj(), c13 = new CountObj(), c14 = new CountObj(), c15 = new CountObj(), c16 = new CountObj(),
-//            c17 = new CountObj(), c18 = new CountObj(), c19 = new CountObj();
     CountObj c1 = new CountObj(), c2 = new CountObj(), c3 = new CountObj();
 
     Calculation calculation;
@@ -112,7 +105,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Notification notification;
 
     public int MINOR = 0;
-    private CountDownTimer timer;
     private boolean running = false;
     public int MAXbeaconNUM = -1, SubspceFingerPrint = -1, RSSISUM8Subspace = -1, chooseSubspace = -1;
     public boolean ThresholdPass = false;
@@ -188,17 +180,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (handlerThread != null) {
-            handlerThread.quit();
-        }
-        if (handler != null) {
-            handler.removeCallbacks(r1);
-        }
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(final Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         if (!mScanner.isScanning()) {
@@ -214,7 +195,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return true;
     }
 
-
     @Override
     public boolean onOptionsItemSelected(final MenuItem item) {
         switch (item.getItemId()) {
@@ -227,6 +207,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
         return true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mScanner.scanLeDevice(-1, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        receive.setText(Integer.toString(COUNT));
+        invalidateOptionsMenu();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (handlerThread != null) {
+            handlerThread.quit();
+        }
+        if (handler != null) {
+            handler.removeCallbacks(r1);
+        }
     }
 
     private void writeFile() {
@@ -249,37 +253,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 FileOutputStream fOut = new FileOutputStream(file);
                 OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
 
-//                myOutWriter.append("No.1, No.2, No.4, No.5, No.6, No.9, No.11, No.12, No.13, No.14, No.15, No.16, No.17, No.18, No.19 \n");
                 myOutWriter.append("No.1, No.2, No.3\n");//minor編號
-
-//                myOutWriter.append(c1.getSize() + "," +c2.getSize() + "," +c4.getSize() + "," +c5.getSize() + "," +c6.getSize() + "," +c9.getSize() + "," +
-//                        c11.getSize() + "," +c12.getSize() + ","+c13.getSize() + "," +c14.getSize() + "," +c15.getSize() + "," +c16.getSize() + "," +
-//                        c17.getSize() + "," +c18.getSize() + "," +c19.getSize() + "\n");
                 myOutWriter.append(c1.getSize() + "," + c2.getSize() + "," + c3.getSize() + "\n");//蒐集訊號數
-
-//                myOutWriter.append(c1.getAvg() + "," + c2.getAvg() + "," + c4.getAvg() + "," + c5.getAvg() + "," + c6.getAvg() + "," + c9.getAvg() + "," +
-//                        c11.getAvg() + "," + c12.getAvg() + "," + c13.getAvg() + "," + c14.getAvg() + "," + c15.getAvg() + "," + c16.getAvg() + "," +
-//                        c17.getAvg() + "," + c18.getAvg() + "," + c19.getAvg() + "\n");
                 myOutWriter.append(c1.getAvg() + "," + c2.getAvg() + "," + c3.getAvg() + "\n");//訊號平均
-
-//                myOutWriter.append(c1.getStandardD() + "," + c2.getStandardD() + "," + c4.getStandardD() + "," + c5.getStandardD() + "," +
-//                        c6.getStandardD() + "," + c9.getStandardD() + "," + c11.getStandardD() + "," + c12.getStandardD() + "," +
-//                        c13.getStandardD() + "," + c14.getStandardD() + "," + c15.getStandardD() + "," + c16.getStandardD() + "," +
-//                        c17.getStandardD() + "," + c18.getStandardD() + "," + c19.getStandardD() + "\n");
                 myOutWriter.append(c1.getStandardD() + "," + c2.getStandardD() + "," + c3.getStandardD() + "\n");//訊號標準差
-
-//                myOutWriter.append(c1.getMax() + "," + c2.getMax() + "," + c4.getMax() + "," + c5.getMax() + "," + c6.getMax() + "," + c9.getMax() + "," +
-//                        c11.getMax() + "," + c12.getMax() + "," + c13.getMax() + "," + c14.getMax() + "," + c15.getMax() + "," + c16.getMax() + "," +
-//                        c17.getMax() + "," + c18.getMax() + "," + c19.getMax() + "\n");
                 myOutWriter.append(c1.getMax() + "," + c2.getMax() + "," + c3.getMax() + "\n");//訊號最大值
-
-//                myOutWriter.append(c1.getMin() + "," + c2.getMin() + "," + c4.getMin() + "," + c5.getMin() + "," + c6.getMin() + "," + c9.getMin() + "," +
-//                        c11.getMin() + "," + c12.getMin() + "," + c13.getMin() + "," + c14.getMin() + "," + c15.getMin() + "," + c16.getMin() + "," +
-//                        c17.getMin() + "," + c18.getMin() + "," + c19.getMin() + "\n");
-                myOutWriter.append(c1.getMin() + "," + c2.getMin() + "," + c3.getMin() + "\n");
-
-                myOutWriter.append("MaxBeacon, RSSI Sum, Choose Subspace" + "\n");
-                myOutWriter.append(MAXbeaconNUM + "," + RSSISUM8Subspace + "," + chooseSubspace);
+                myOutWriter.append(c1.getMin() + "," + c2.getMin() + "," + c3.getMin() + "\n");//訊號最小值
 
                 myOutWriter.append("\n\nNo.1\n");
                 for (int i = 0; i < c1.getSize(); i++) {
@@ -308,42 +287,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void cleanCountObj() {
-//        c1 = null;
-//        c2 = null;
-//        c4 = null;
-//        c5 = null;
-//        c6 = null;
-//        c9 = null;
-//        c11 = null;
-//        c12 = null;
-//        c13 = null;
-//        c14 = null;
-//        c15 = null;
-//        c16 = null;
-//        c17 = null;
-//        c18 = null;
-//        c19 = null;
         c1 = null;
         c2 = null;
         c3 = null;
     }
 
     private void separateBeacon() {
-//        c1 = new CountObj();
-//        c2 = new CountObj();
-//        c4 = new CountObj();
-//        c5 = new CountObj();
-//        c6 = new CountObj();
-//        c9 = new CountObj();
-//        c11 = new CountObj();
-//        c12 = new CountObj();
-//        c13 = new CountObj();
-//        c14 = new CountObj();
-//        c15 = new CountObj();
-//        c16 = new CountObj();
-//        c17 = new CountObj();
-//        c18 = new CountObj();
-//        c19 = new CountObj();
         c1 = new CountObj();
         c2 = new CountObj();
         c3 = new CountObj();
@@ -358,42 +307,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (wtdArray.get(i).Minor == 3) {
                 c3.inputArray(wtdArray.get(i).RSSI);
             }
-//            if (wtdArray.get(i).Minor == 5) {
-//                c5.inputArray(wtdArray.get(i).RSSI);
-//            }
-//            if (wtdArray.get(i).Minor == 6) {
-//                c6.inputArray(wtdArray.get(i).RSSI);
-//            }
-//            if (wtdArray.get(i).Minor == 9) {
-//                c9.inputArray(wtdArray.get(i).RSSI);
-//            }
-//            if (wtdArray.get(i).Minor == 11) {
-//                c11.inputArray(wtdArray.get(i).RSSI);
-//            }
-//            if (wtdArray.get(i).Minor == 12) {
-//                c12.inputArray(wtdArray.get(i).RSSI);
-//            }
-//            if (wtdArray.get(i).Minor == 13) {
-//                c13.inputArray(wtdArray.get(i).RSSI);
-//            }
-//            if (wtdArray.get(i).Minor == 14) {
-//                c14.inputArray(wtdArray.get(i).RSSI);
-//            }
-//            if (wtdArray.get(i).Minor == 15) {
-//                c15.inputArray(wtdArray.get(i).RSSI);
-//            }
-//            if (wtdArray.get(i).Minor == 16) {
-//                c16.inputArray(wtdArray.get(i).RSSI);
-//            }
-//            if (wtdArray.get(i).Minor == 17) {
-//                c17.inputArray(wtdArray.get(i).RSSI);
-//            }
-//            if (wtdArray.get(i).Minor == 18) {
-//                c18.inputArray(wtdArray.get(i).RSSI);
-//            }
-//            if (wtdArray.get(i).Minor == 19) {
-//                c19.inputArray(wtdArray.get(i).RSSI);
-//            }
         }
     }
 
@@ -414,8 +327,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 separateBeacon();//初始化
                 positioning();//定位
 
+                //-------嗶生提醒量測完畢
                 ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, ToneGenerator.MAX_VOLUME);
                 toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
+                //-------------------------------------------
+
                 manger.notify(1, notification);
                 cleanCountObj();
 
@@ -449,6 +365,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, ToneGenerator.MAX_VOLUME);
                 toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
                 //-------------------------------------------
+
                 running = false;
                 runOnUiThread(new Runnable() {
                     @Override
@@ -527,59 +444,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     running = false;
                     temp = 0;
                 }
-/*
-                if (receive.isChecked() && !loopChk.isChecked()) {//不持續定位檢查有無要寫檔
-//                    drawView.draw = false;
-//                    COUNT = Integer.parseInt(mSetTime.getText().toString());
-                    //receive.setText(Integer.toString(COUNT));
-                    wtdArray = null;
-                    wtdArray = new ArrayList<WriteFileData>();
-                    calculation = new Calculation();
-                    if (!running) {
-                        running = true;
-                        timer = new CountDownTimer(COUNT * 1000, 1000) {
-                            @Override
-                            public void onFinish() {
-                                separateBeacon();//初始化
-                                positioning();//定位
-
-                                if (Ifwrite.isChecked()) {//是否寫檔
-                                    writeFile();
-                                }
-                                //-------嗶生提醒量測完畢
-                                ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, ToneGenerator.MAX_VOLUME);
-                                toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
-                                //-------------------------------------------
-                                running = false;
-                                receive.setChecked(false);
-                                receive.setText(Integer.toString(COUNT));
-                                manger.notify(1, notification);
-                                cleanCountObj();
-                            }
-
-                            @Override
-                            public void onTick(long millisUntilFinished) {
-                                receive.setText(Long.toString(millisUntilFinished / 1000));
-                            }
-
-                        }.start();
-                    }
-                } else {
-                    timer.cancel();
-                    receive.setChecked(false);
-                    receive.setText(Integer.toString(COUNT));
-                    running = false;
-                }
-*/
                 break;
         }
     }
 
     private void positioning() {
-//        double[] calbeacon = {c1.getAvg(), c2.getAvg(), c4.getAvg(),
-//                c5.getAvg(), c6.getAvg(), c9.getAvg(), c11.getAvg(),
-//                c12.getAvg(), c13.getAvg(), c14.getAvg(), c15.getAvg(),
-//                c16.getAvg(), c17.getAvg(), c18.getAvg(), c19.getAvg()};
         double[] calbeacon = {c1.getAvg(), c2.getAvg(), c3.getAvg()};
 
 //			MAXbeaconNUM = calculation.calculateMAXbeacon(calbeacon); //取最大Beacon訊號值所在當選擇的子區域
@@ -616,19 +485,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         drawView.draw = true;
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mScanner.scanLeDevice(-1, false);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        receive.setText(Integer.toString(COUNT));
-        invalidateOptionsMenu();
     }
 
     private void startScan() {
